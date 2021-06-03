c2fb061fec5774b18d49f52fecf3cd71f2000608
ask <tjdrud3632@naver.com>
2021-06-03 21:00:33 +0900

    6/3

--- src/main/java/com/example/demo/config/SocketHandler.java
+++ src/main/java/com/example/demo/config/SocketHandler.java
@@ -1,15 +1,17 @@
 package com.example.demo.config;
 
-import jdk.nashorn.internal.parser.JSONParser;
-import jdk.nashorn.internal.runtime.ParserException;
+import java.util.HashMap;
+
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.util.HashMap;
+
 
 //웹소켓 구현체
 @Component
@@ -38,7 +40,10 @@ public class SocketHandler extends TextWebSocketHandler {
         //소켓 연결되면 동작
         super.afterConnectionEstablished(session);
         sessionMap.put(session.getId(), session);
-        //map의 put이 뭔지 찾기
+        JSONObject obj = new JSONObject();
+        obj.put("type", "getId");   //생성된 세션을 저장하면 '발신 메세지'의 타입은 getId로 명시 후
+        obj.put("sessionId", session.getId());  //생성된 세션 ID값을 클라이언트단으로 발송한다.
+        session.sendMessage(new TextMessage(obj.toJSONString()));
     }
 
     @Override
@@ -48,17 +53,15 @@ public class SocketHandler extends TextWebSocketHandler {
         super.afterConnectionClosed(session, status);
     }
 
-    @Override
-    private static JSONObject JsonToObjectParser(String jsonStr){
+    private static JSONObject JsonToObjectParser(String jsonStr) { //json 파일이 들어오면 파싱을 해주는 함수 JsonToObjectParser
         JSONParser parser = new JSONParser();
         JSONObject obj = null;
-        try{
+        try {
             obj = (JSONObject) parser.parse(jsonStr);
-        }catch (ParserException e){
+        } catch (ParseException e) {
             e.printStackTrace();
         }
         return obj;
-
     }
 
 }
